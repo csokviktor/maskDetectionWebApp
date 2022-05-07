54c5cf94398f18280a1eeaa514e5114e16af1ab7
Viktor Csok <csokviktor@gmail.com>
2022-05-07 11:52:57 +0200

    notification categories handling

--- webpage/main.py
+++ webpage/main.py
@@ -16,13 +16,11 @@ def startApp(inpDict, inpLock, procDict, procLock):
 
 if __name__ == '__main__':
     inputDict, processedDict, inputLock, processedLock, pool = initProcessObjects()
-    # pool.apply_async(runSubscriber, args=("tcp://127.0.0.1:5554", 0, managerList, managerLock))
-    # pool.apply_async(runSubscriber, args=("tcp://127.0.0.1:5555", 1, managerList, managerLock))
     pool.apply_async(startApp, args=(inputDict, inputLock, processedDict, processedLock))
     # pool.apply_async(showImage, args=(inputDict, inputLock))
     # pool.apply_async(showProcessedImage, args=(processedDict, processedLock))
     setup_args = setupDetection(
-        source="client",
-        weights=r"C:\Users\csokviktor\Desktop\maskdetection\best_maskdetv2_2_strip.pt",
-        deviceName='0')
+       source="client",
+       weights=r"C:\Users\csokviktor\Desktop\maskdetection\best_maskdetv2_2_strip.pt",
+       deviceName='0')
     processImage(inputDict, processedDict, processedLock, *setup_args)
--- webpage/notification.py
+++ webpage/notification.py
@@ -15,11 +15,16 @@ notification = Blueprint('notification', __name__)
 @notification.route('/notification-management', methods=['GET'])
 def notification_management():
     current_categories = [cat.category for cat in SelectedCategories.query.all()]
+    previous_notifications = [
+        f'[{notifications.ts}] Camera: {notifications.cameraID} Status: {notifications.status}'
+        for notifications
+        in Notifications.query.all()]
     return render_template(
         'notificationmanagement.html',
         user=current_user,
         available_categories=initialization.available_categories,
-        current_categories=current_categories
+        current_categories=current_categories,
+        notifications=previous_notifications
     )
 
 
@@ -35,6 +40,11 @@ def add_new_notification():
     res = all(elem in required_fields for elem in content.keys())
     if not res:
         return Response('Not all keys are satisfied', status=500)
+
+    current_categories = [cat.category for cat in SelectedCategories.query.all()]
+    print(current_categories, content['status'])
+    if content['status'] not in current_categories:
+        return Response(status=200)
 
     # Get the latest notification from the same camera with same topic
     latest_notification = Notifications.query.filter_by(
@@ -61,12 +71,12 @@ def add_new_notification():
 def get_notification():
     notifications = Notifications.query.all()
     temp_dict = {'notifications': []}
-    for notification in notifications:
+    for notific in notifications:
         temp_dict['notifications'].append(jsonify(
-            id=notification.id,
-            ts=notification.ts,
-            cameraID=notification.cameraID,
-            status=notification.status
+            id=notific.id,
+            ts=notific.ts,
+            cameraID=notific.cameraID,
+            status=notific.status
         ).get_json())
     return Response(json.dumps(temp_dict), status=200, content_type='application/json')
 
@@ -75,4 +85,13 @@ def get_notification():
 def update_categories():
     data = json.loads(request.data)
     category = data['categoryName']
+    category_check = SelectedCategories.query.filter_by(category=category).first()
+    if category_check:
+        initialization.db.session.delete(category_check)
+    else:
+        new_category = SelectedCategories(
+            category=category
+        )
+        initialization.db.session.add(new_category)
+    initialization.db.session.commit()
     return jsonify({})
